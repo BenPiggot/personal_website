6aed55407053aa2d2d11a4e963a76a0d00da6800
ben <wbpiggot@gmail.com>
2018-05-28 16:56:40 -0700

    integrating material UI

--- app/static/App.tsx
+++ app/static/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import injectTapEventPluginRequire = require("react-tap-event-plugin");
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiInput: { 
+      root: { // Name of the rule
+        color: 'cadetblue',
+        borderColor: 'cadeblue'
+      },
+    }
+  },
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#6D9C9F',
+    }
+  }
+});
 
 class App extends React.Component<{},{}> {
   render() {
     return (
-      <div>Testing...</div>
+      <MuiThemeProvider theme={theme}>
+        <Input color={'cadetblue'} />
+      </MuiThemeProvider>
     )
   }
 }
